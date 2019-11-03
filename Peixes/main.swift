487779e8641a52e4fb027ca23dca43f5ff78c8b3
Elias Paulino <eliaspaulinoandrade@gmail.com>
2019-11-03 10:43:02 -0300

    Initial Commit

---
+++ Peixes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Peixes
+//
+//  Created by Elias Paulino on 03/11/19.
+//  Copyright © 2019 Elias Paulino. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
